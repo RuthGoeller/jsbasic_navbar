1be8fecf084a8b624a2d9c00c70d13d5ffb6a193
Ruth Goeller <ruthgoeller@Ruths-Air.lan>
2023-10-06 22:00:49 -0500

    nav

---
+++ app.js
@@ -0,0 +1,8 @@
+
+
+
+const toggle = document.querySelector('.nav-toggle')
+const links = document.querySelector('.links')
+
+toggle.addEventListener('click', ()=>links.classList.toggle('show-links')
+)
